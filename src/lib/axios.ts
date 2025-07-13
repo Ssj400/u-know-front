b11ea2e159d5922b0b18872c0a167ff53ae8277e
ssj400 <gafetel2017@gmail.com>
2025-07-13 09:32:04 -0400

    Enhance API configuration: add request interceptor to include 'access_token' in authorization headers

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,17 @@
 import axios from "axios"; 
+import Cookies from "js-cookie";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
+});
+
+api.interceptors.request.use((config) => {
+  const token = Cookies.get("access_token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default api;
