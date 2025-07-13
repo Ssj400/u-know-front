bce5ab23715d51b75b01cf396918d087e183c3aa
ssj400 <gafetel2017@gmail.com>
2025-07-13 11:32:12 -0400

    Refactor PostsPage layout and enhance Navbar styling for better structure

--- src/app/main/posts/page.tsx
+++ src/app/main/posts/page.tsx
@@ -1,22 +1,18 @@
+"use client";
 
-'use client';
-
-import { useEffect, useState } from 'react';
-import api from '@/lib/axios';
-import { Post } from '@/types/post';
-import PostCard from '@/components/post-card';
-import Navbar from '@/components/navbar';
-import AddPostButton from '@/components/add-post-button';
-
-
-
+import { useEffect, useState } from "react";
+import api from "@/lib/axios";
+import { Post } from "@/types/post";
+import PostCard from "@/components/post-card";
+import Navbar from "@/components/navbar";
+import AddPostButton from "@/components/add-post-button";
 
 export default function PostsPage() {
   const [posts, setPosts] = useState<Post[]>([]);
 
   useEffect(() => {
     async function fetchPosts() {
-      const res = await api.get('/posts');
+      const res = await api.get("/posts");
       setPosts(res.data);
     }
 
@@ -24,13 +20,15 @@ export default function PostsPage() {
   }, []);
 
   return (
-    <div className="p-6 space-y-4">
+    <div>
       <Navbar></Navbar>
-      <AddPostButton />
-      <h1 className="text-2xl font-bold">All Posts</h1>
-      {posts.map(post => (
-        <PostCard key={post.id} post={post} />
-      ))}
+      <div className="p-6 space-y-4">
+        <AddPostButton />
+        <h1 className="text-2xl font-bold">All Posts</h1>
+        {posts.map((post) => (
+          <PostCard key={post.id} post={post} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,7 +3,7 @@ import InfoButton from '@/components/ui/info-button/info-button';
 
 export default function Navbar() {
   return (
-    <header className="flex items-center justify-between p-6 bg-transparent">
+    <header className="flex items-center justify-between p-6 bg-transparent border-b border-gray-200 border-w-200">
       <div className="flex-1">
         <h1 className="text-3xl font-bold bg-transparent">Uknow</h1>
       </div>
@@ -15,7 +15,7 @@ export default function Navbar() {
             </Link>
           </li>
           <li>
-            <Link href="/about" className="text-lg hover:text-blue-400 transition-colors">
+            <Link href="/main/posts" className="text-lg hover:text-blue-400 transition-colors">
               Explore
             </Link>
           </li>
@@ -30,5 +30,6 @@ export default function Navbar() {
         <InfoButton />
       </div>
     </header>
+    
   );
 }
