425d42ceffe085c558b549a1ee21efe9b6d52fea
ssj400 <gafetel2017@gmail.com>
2025-07-21 23:53:28 -0400

    Enhance EditProfile component with user data fetching and form handling

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -7,7 +7,7 @@ export default function ProfilePage() {
   return (
     <div className="bg-gray-900 min-h-screen">
         <Navbar />
-      <section className="w-full max-w-4xl mx-auto p-6 flex flex-col items-center">
+      <section className="w-full max-w-4xl mx-auto p-6 flex flex-col items-center h-[calc(100vh-100px)]">
         <EditProfile />
       </section>
       
--- src/components/profile/edit-profile.tsx
+++ src/components/profile/edit-profile.tsx
@@ -1,42 +1,81 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Input } from "../ui/input";
 import api from "@/lib/axios";
 import { UserStats } from "@/types/user";
 
-
-
 export default function EditProfile() {
-    const [user, setUser] = useState<UserStats | null>(null);
+  const [user, setUser] = useState<UserStats | null>(null);
+  const [nickname, setNickname] = useState("");
+  const [email, setEmail] = useState("");
+  const [bio , setBio] = useState("");
+  const formFilled = useRef(false);
 
-    useEffect(() => {
-        async function fetchUserProfile() {
-            const { data } = await api.get("/users/profile");
-            setUser(data);
-        }
+  useEffect(() => {
+    async function fetchUserProfile() {
+      const { data } = await api.get("/users/profile");
+      setUser(data);
+    }
 
-        fetchUserProfile();
-    }, []);
+    fetchUserProfile();
+  }, []);
+
+  useEffect(() => {
+    formFilled.current = nickname !== "" || email !== "" || bio !== "";
+    console.log("Form filled status:", formFilled.current);
+  }, [nickname, email, bio]);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const updatedData = {
+      nickname: nickname !== "" ? nickname : user?.nickname,
+      email: email !== "" ? email : user?.email,
+      bio: bio !== "" ? bio : user?.bio,
+    };
+
+    const data = { ...updatedData };
+  };
 
   return (
     <div className="bg-gray-900 w-[50%] text-white p-6 rounded-lg space-y-4 flex flex-col justify-center h-full">
-      <label>
-        Nickname
-        <Input placeholder={user?.nickname} />
-      </label>
-      <label>
-        Email
-        <Input placeholder={user?.email} />
-      </label>
-      <label>
-        Bio
-        <Input placeholder={user?.bio === "" ? "Enter your bio" : user?.bio} />
-      </label>
-      <label>
-        Role
-        <Input placeholder={user?.role} readOnly/>
-      </label>
+      <div className="bg-blue-500 text-white rounded-full h-20 w-20 flex items-center justify-center text-4xl font-bold mr-3 self-center mb-4">
+        {user?.nickname.charAt(0).toUpperCase()}
+      </div>
+      <form onSubmit={handleSubmit} className="space-y-4 flex flex-col w-full">
+        <label>
+          Nickname
+          <Input placeholder={user?.nickname} onChange={(e) => setNickname(e.target.value)} />
+        </label>
+        <label>
+          Email
+          <Input placeholder={user?.email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <label>
+          Bio
+          <Input
+            placeholder={user?.bio === "" ? "Enter your bio" : user?.bio}
+            onChange={(e) => setBio(e.target.value)}
+          />
+        </label>
+        <label>
+          Role
+          <Input placeholder={user?.role} readOnly />
+        </label>
+        {
+          formFilled.current ? (
+              <button className="bg-blue-500  text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors cursor-pointer active:bg-blue-700">
+          Save Changes
+        </button>
+          ) : (
+            <button className="bg-gray-500 text-white px-4 py-2 rounded cursor-not-allowed" disabled>
+              Save Changes
+            </button>
+          )
+          }
+        
+      </form>
     </div>
   );
 }
