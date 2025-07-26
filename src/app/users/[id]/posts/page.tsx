3eb468d8875134760cb3d878f1a20db63677ab02
ssj400 <gafetel2017@gmail.com>
2025-07-25 22:09:23 -0400

    Fetch user profile and display nickname in UserPostPage header

--- src/app/users/[id]/posts/page.tsx
+++ src/app/users/[id]/posts/page.tsx
@@ -5,10 +5,11 @@ import Navbar from "@/components/navbar";
 import { useParams } from "next/navigation";
 import { useUserPosts } from "@/hooks/useUserPosts";
 import PostCard from "@/components/post-card";
+import { useFetchUserProfile } from "@/hooks/fetchUserProfile";
 
 export default function UserPostPage() {
   const { id } = useParams();
-
+  const { user } = useFetchUserProfile();
   const { posts, loading, error } = useUserPosts(Number(id));
 
   if (loading) return <p className="p-6 text-gray-500">Loading...</p>;
@@ -19,7 +20,7 @@ export default function UserPostPage() {
       <Navbar />
       <div className="container mx-auto px-4 py-8">
         <div className="flex justify-between items-center mb-6">
-          <h1 className="text-3xl font-bold">All User&apos;s Posts</h1>
+          <h1 className="text-3xl font-bold">All <strong className="text-blue-400">{user?.nickname}</strong> Posts</h1>
         </div>
         <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {posts.map((post) => (
