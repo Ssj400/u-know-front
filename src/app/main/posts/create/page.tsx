1bf03aeddef4dbf488d9e61fb396ae32bd477e15
ssj400 <gafetel2017@gmail.com>
2025-07-27 22:53:53 -0400

    Enhance CreatePostPage: update background color and integrate Navbar component

--- src/app/main/posts/create/page.tsx
+++ src/app/main/posts/create/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import CreatePostForm from "@/components/create-post-form";
+import Navbar from "@/components/navbar";
 import { useAuthRedirect } from "@/hooks/useAuthRedirect";
 
 export default function CreatePostPage() {
   useAuthRedirect();
 
   return (
-    <div className="bg-gray-950 min-h-screen">
+    <div className="bg-gray-900 min-h-screen">
+      <Navbar />
       <div className="container mx-auto px-4 py-8">
         <h1 className="text-3xl font-bold mb-8 text-center">
           Create a New Post
