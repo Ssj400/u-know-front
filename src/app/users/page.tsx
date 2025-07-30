01de25d1f6a36868e1de22be1cd9e8e1d47db9e8
ssj400 <gafetel2017@gmail.com>
2025-07-29 20:09:10 -0400

    Enhance UsersPage and useFetchUsers hook: add loading and error handling

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Loading from "@/components/loading";
 import Navbar from "@/components/navbar";
 import { useFetchUsers } from "@/hooks/useFetchUsers";
 
@@ -6,21 +7,19 @@ import { useFetchUsers } from "@/hooks/useFetchUsers";
 
 
 export default function UsersPage() {
-    const { users } = useFetchUsers();
-    if (!users || users.length === 0) {
-        return <p className="p-6 text-red-500">No users found</p>;
-    }
+    const { users, loading: usersLoading, error: usersError } = useFetchUsers();
 
   return (
     <div className="w-full bg-gray-900 min-h-screen">
       <Navbar />
-      { users.map(user => (
-        <div key={user.id} className="p-6 border-b border-gray-800">
+      {usersLoading && <Loading />}
+      {usersError && <p className="p-6 text-red-500">{usersError}</p>}
+      {users.map(user => (
+        <div key={user.id} className="p-6 border-b border-gray-800 cursor-pointer hover:bg-gray-700"  onClick={() => window.location.href = `/users/${user.id}/posts`}>
           <h2 className="text-xl font-bold text-white">{user.nickname}</h2>
-          <p className="text-gray-400">{user.email}</p>
+          <p className="text-gray-400">{user.bio || "No bio found"}</p>
         </div>
       ))}
-
     </div>
   );
 }   
--- src/hooks/useFetchUsers.ts
+++ src/hooks/useFetchUsers.ts
@@ -6,19 +6,25 @@ import api from "@/lib/axios";
 
 export function useFetchUsers() {
   const [users, setUsers] = useState<UserStats[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     async function fetchUsers() {
+      setLoading(true);
       try {
         const res = await api.get("/users");
         setUsers(res.data);
       } catch (error) {
         console.error("Error fetching users:", error);
+        setError("Unexpected error occurred while fetching users.");
+      } finally {
+        setLoading(false);
       }
     }
 
     fetchUsers();
   }, []);
 
-  return { users };
+  return { users, loading, error };
 }
