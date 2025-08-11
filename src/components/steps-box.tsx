4cadad07147f41b91b6ab146743e06983e08d08d
ssj400 <gafetel2017@gmail.com>
2025-08-10 23:22:04 -0400

    Enhance FeatureBox: change background color to gray for better visibility

--- src/components/steps-box.tsx
+++ src/components/steps-box.tsx
@@ -7,7 +7,7 @@ interface FeatureBoxProps {
 
 export default function FeatureBox({ title, description }: FeatureBoxProps) {
   return (
-    <div className="flex flex-col items-center text-center p-4 bg-transparent rounded-lg shadow-lg">
+    <div className="flex flex-col items-center text-center p-4 bg-gray-800 rounded-lg shadow-lg">
       <h3 className="text-xl font-bold mb-2 w-60">{title}</h3>
       <p className="text-gray-400 w-90">{description}</p>
     </div>
