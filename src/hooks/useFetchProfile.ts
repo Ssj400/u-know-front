be2c40adfab86f8e485790ce4f932b4dea64960d
ssj400 <gafetel2017@gmail.com>
2025-08-11 22:28:01 -0400

    Enhance useFetchUserProfile: improve error handling and loading state management

--- src/hooks/useFetchProfile.ts
+++ src/hooks/useFetchProfile.ts
@@ -2,25 +2,28 @@ import api from "@/lib/axios";
 import { UserStats } from "@/types/user";
 import { useEffect, useState } from "react";
 
-
 export function useFetchUserProfile(userId: string) {
   const [user, setUser] = useState<UserStats | null>(null);
-
-  
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     async function fetchUserProfile() {
-    try {
-      const { data } = await api.get(`/users/profile/${userId}/`);
-      setUser(data);
-    } catch (error) {
-      setUser(null);
+      setLoading(true);
+      try {
+        const { data } = await api.get(`/users/profile/${userId}/`);
+        setUser(data);
+      } catch (error) {
+        setUser(null);
         console.error("Failed to fetch user profile:", error);
+        setError("Failed to fetch user profile. Please try again later.");
+      } finally {
+        setLoading(false);
+      }
     }
-  }
 
     fetchUserProfile();
-}, [userId]);
+  }, [userId]);
 
-  return { user, refetch: useFetchUserProfile };
+  return { user, refetch: useFetchUserProfile, error, loading };
 }
